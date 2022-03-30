db120a6a3fadf746d639cc9c4db3e9275deaa21a
AndreTill <iandretill@icloud.com>
2022-03-30 04:59:24 +0300

    Initial Commit

---
+++ cxcx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cxcx
+//
+//  Created by Андрей Безсонный on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
